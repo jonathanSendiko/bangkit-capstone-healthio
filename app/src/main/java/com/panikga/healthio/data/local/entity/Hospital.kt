3c7b820fce6514fe6e79d4b62b4291fc0dfed69d
nathan <faustinenathaniel@gmail.com>
2021-05-18 13:33:44 +0700

    sort by rating

---
+++ app/src/main/java/com/panikga/healthio/data/local/entity/Hospital.kt
@@ -0,0 +1,16 @@
+package com.panikga.healthio.data.local.entity
+
+data class Hospital(
+    var akreditasi: String? = "",
+    var alamat: String? = "",
+    var district : String? = "",
+    var jenis: String? = "",
+    var kelas: String? = "",
+    var kode_rs: String? = "",
+    var lat: String? = "",
+    var long : String? = "",
+    var nama: String? = "",
+    var pemilik: String? = "",
+    var rating_avg: String? = "",
+    var rating_count: String? = ""
+)
